ada880b0d907e368957dd2c9465d949a7a37cb0f
Tommy Arbuthnot <tommyarbuthnot@Thomass-MacBook-Pro.local>
2018-03-10 16:52:49 -0600

    game logic more

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,26 @@
+
+//arrays for storing cards
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped" + " " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped" + " " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+var checkForMatch = function() {
+	if (cardsInPlay.length === 1) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 } 
+
+//function for printing player actions
+var flipCard = function(cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+
+ 
+
